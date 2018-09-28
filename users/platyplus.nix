b386cdf466653b538f653cca9abf5dcbe1e4ca40
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-09-28 19:42:29 +0200

    platyplus user

--- users/platyplus.nix
+++ users/platyplus.nix
@@ -1,11 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.extraUsers.msfocb = {
+  users.extraUsers.platyplus = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     # python2 -c 'import crypt, getpass,os,base64; print crypt.crypt(getpass.getpass(), "$6$"+base64.b64encode(os.urandom(16))+"$")'
-    hashedPassword = "$6$fdEP2xGs$2qw.bg8Mb5ohQvIl3UAbr65Mi9C.m4qXs9R.Vc7TqZVemxt3AfF5oQNNZZwbyYd/MrVd2UMGjW4jQAcYFvgLJ/";
+    # openssl passwd -1 -salt xyz password
+    hashedPassword = "$1$4=diwou7$lfa6ztu7qd3agYtGlxqJl/";
     openssh.authorizedKeys.keyFiles = [];
   };
 }
