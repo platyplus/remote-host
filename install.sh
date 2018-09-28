c19e782b408097797711fef5cec0a3ac89a4510d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-09-28 15:35:35 +0200

    wrong column name on finding the device

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ sed -i -e 's/{{hostname}}/'"$NEW_HOSTNAME"'/g' /mnt/etc/nixos/settings.nix
 
 # Find a stable device name for grub and append it to the settings file for copy/paste:
 # Then you can add the path to the grub.device setting.
-DEVICEID=`ls -l /dev/ | grep "${TGTDEV##*/}$" | awk '{print $9}'`
+DEVICEID=`ls -l /dev/ | grep "${TGTDEV##*/}$" | awk '{print $10}'`
 sed -i -e 's/{{device}}/'"${DEVICEID//\//\\/}"'/g' /mnt/etc/nixos/settings.nix
 
 TIMEZONE="Europe/Brussels" # TODO prompt
