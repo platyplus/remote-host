b757c56ffb6da95f73c93dafc5213061a3739da4
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-09-28 19:58:02 +0200

    new password

--- users/platyplus.nix
+++ users/platyplus.nix
@@ -5,8 +5,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     # python2 -c 'import crypt, getpass,os,base64; print crypt.crypt(getpass.getpass(), "$6$"+base64.b64encode(os.urandom(16))+"$")'
-    # openssl passwd -1 -salt xyz password
-    hashedPassword = "$1$4=diwou7$lfa6ztu7qd3agYtGlxqJl/";
+    hashedPassword = "$6H00rXZYGlxU";
     openssh.authorizedKeys.keyFiles = [];
   };
 }
