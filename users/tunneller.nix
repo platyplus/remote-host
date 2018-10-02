2fd0ead8a3977c739ff8abaf98986489f0f50ecf
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-02 21:09:04 +0200

    minor cleanup

--- users/tunneller.nix
+++ users/tunneller.nix
@@ -6,14 +6,7 @@
     isSystemUser = true;
     shell = pkgs.nologin;
     openssh.authorizedKeys.keyFiles = [
-      ../keys/didier
-      ../keys/dirk
-      ../keys/joana
-      ../keys/kathy
-      ../keys/manu
-      ../keys/mohammad
-      ../keys/ramses
-      ../keys/thierry
+      ../keys/pilou
       ../keys/yusuph
     ];
   };
