46010874b5fb77c54930f72b36249347978b6075
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-04 21:38:48 +0200

    minor remove, just to see

--- system-sync.nix
+++ system-sync.nix
@@ -24,7 +24,6 @@
                 ${pkgs.git}/bin/git remote add origin "https://github.com/${(import ./settings.nix).github_repository}"
             fi
             ${pkgs.git}/bin/git fetch
-            echo "ici"
             if [[ $(${pkgs.git}/bin/git rev-parse HEAD) != $(${pkgs.git}/bin/git rev-parse @{u}) ]]; then
                 ${pkgs.git}/bin/git reset --hard HEAD
                 ${pkgs.git}/bin/git checkout --force --track origin/master  # Force to overwrite local files
