ea788859e838d146c385a315bbc1e2ff12f4755f
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-04 20:10:11 +0200

    prep syn script

--- sync.sh
+++ sync.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 GITHUB_REPO=platyplus/remote-host
-REMOTE_SHA=`git ls-remote "https://github.com/$GITHUB_REPO" | grep HEAD | awk '{print $1}'`
-LOCAL_SHA=`git ls-remote /etc/nixos | grep HEAD | awk '{print $1}'`
 cd /etc/nixos
 if [ ! -d /etc/nixos/.git ]; then
     git init
-    git remote add origin https://github.com/platyplus/remote-host
+    git remote add origin "https://github.com/$GITHUB_REPO"
 fi
-if [ "$REMOTE_SHA" != "$LOCAL_SHA" ]; then
-    git fetch
+fetch=`git fetch`
+if [[ -d $fetch ]]; then
     git checkout --force --track origin/master  # Force to overwrite local files
     git pull --rebase
+    nixos-rebuild switch --upgrade
 fi
