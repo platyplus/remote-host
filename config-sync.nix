5739eded145b91b3a957e5291fb029bfc18b40c4
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-05 15:09:09 +0200

    another systemd attempt

--- config-sync.nix
+++ config-sync.nix
@@ -24,18 +24,26 @@
                 ${pkgs.git}/bin/git remote add origin "https://github.com/${(import ./settings.nix).github_repository}"
             fi
             ${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git pull
+            ${config.system.build.nixos-rebuild}/bin/nixos-rebuild switch --upgrade --no-build-output
+        '';
+        description = "NixOS Upgrade";
+
+        restartIfChanged = false;
+        unitConfig.X-StopOnRemoval = false;
+
+        serviceConfig.Type = "oneshot";
+
+        environment = config.nix.envVars //
+            { inherit (config.environment.sessionVariables) NIX_PATH;
+            HOME = "/root";
+            } // config.networking.proxy.envVars;
+
+        path = [ pkgs.gnutar pkgs.xz.bin config.nix.package.out ];
+
+        script = ''
+            
         '';
 
-        wantedBy = [ "default.target" ];
-        };
-
-        systemd.timers.syncSystem = {
-        timerConfig = {
-            Unit = "syncSystem.service";
-            OnCalendar = "*-*-* *:00:00";
-        };
-        wantedBy = [ "timers.target" ];
-        };
-        
+        startAt = optional "*-*-* *:*:00";        
 }
 
