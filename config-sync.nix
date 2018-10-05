1eb7de309c71d6dd134a13dd7fc2e3a872874bf8
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-05 13:25:00 +0200

    simplify script

--- config-sync.nix
+++ config-sync.nix
@@ -11,29 +11,20 @@
 { config, lib, pkgs, ... }:
 
 {
-    environment.systemPackages = [
-        (pkgs.writeShellScriptBin "update-nixos-configuration" ''
+    systemd.services.syncSystem = {
+        # environment = config.nix.envVars //
+        # { inherit (config.environment.sessionVariables) NIX_PATH;
+        #   HOME = "/root";
+        # } // config.networking.proxy.envVars;
+
+        script = ''
             cd /etc/nixos
             if [ ! -d .git ]; then
                 ${pkgs.git}/bin/git init .
                 ${pkgs.git}/bin/git remote add origin "https://github.com/${(import ./settings.nix).github_repository}"
             fi
             ${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git pull
-            # if [[ $(${pkgs.git}/bin/git rev-parse HEAD) != $(${pkgs.git}/bin/git rev-parse @{u}) ]]; then
-            #     ${pkgs.git}/bin/git reset --hard HEAD
-            #     ${pkgs.git}/bin/git checkout --force --track origin/master  # Force to overwrite local files
-            #     ${pkgs.git}/bin/git pull --rebase
-            # fi
-        '')
-    ];
-
-    systemd.services.syncSystem = {
-        environment = config.nix.envVars //
-        { inherit (config.environment.sessionVariables) NIX_PATH;
-          HOME = "/root";
-        } // config.networking.proxy.envVars;
-
-        script = "update-nixos-configuration";
+        '';
 
         wantedBy = [ "default.target" ];
         };
