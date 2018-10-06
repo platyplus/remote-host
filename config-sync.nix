58f29807878551a5ca1e55c3e10d121af1a6adff
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-06 21:12:49 +0200

    diffutils

--- config-sync.nix
+++ config-sync.nix
@@ -15,7 +15,7 @@
         description = "System sync from github configuration repository";
         script = ''
             #!${pkgs.stdenv.shell}
-            echo ${pkgs}
+            echo ${pkgs.diffutils}
             cd /etc/nixos
             if [ ! -d .git ]; then
                 ${pkgs.git}/bin/git init .
