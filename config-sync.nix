d09c4d3b85682169cf4152bd3fd59fc9330dd051
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-06 20:33:18 +0200

    curl mute if error

--- config-sync.nix
+++ config-sync.nix
@@ -28,13 +28,12 @@
             echo "Connecting to the cloud server to get the settings file..."
             endpoint=${(import ./settings.nix).api_endpoint}/host-settings
             credentials="${(import ./settings.nix).hostname}:$(cat ./local/service.pwd)"
-            status_code=$(curl --fail -u "$credentials" -H 'Cache-Control: no-cache' --silent -o /dev/null --head --write-out '%{http_code}\n' $endpoint)
-            if [ $status_code == "200" ]; then
-                SETTINGS=$(${pkgs.curl}/bin/curl --fail -u "$credentials" -H 'Cache-Control: no-cache' --silent $endpoint)
+            SETTINGS=$(${pkgs.curl}/bin/curl -s -u "$credentials" -H 'Cache-Control: no-cache' --silent $endpoint)
+            if [ -n "$SETTINGS" ]; then
                 echo "prediff"
                 changes=$(diff <(echo $SETTINGS) /etc/nixos/settings.nix)
                 echo "postDiff"
-                if [ -n "$SETTINGS" ] && [ "$SETTINGS" != 'null' ] && [[ -Z $changes ]]; then
+                if [[ -z $changes ]]; then
                     echo "$SETTINGS" > /etc/nixos/settings.nix
                     echo "Pushed the new configuration from the server."
                     touch /var/sync-config.lock
