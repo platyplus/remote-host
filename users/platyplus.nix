1e43d65f313ace7570a80c5cb562b5863a30bd25
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2018-10-30 20:14:31 +0100

    change platyplus hash

--- users/platyplus.nix
+++ users/platyplus.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     # python2 -c 'import crypt, getpass,os,base64; print crypt.crypt(getpass.getpass(), "$6$"+base64.b64encode(os.urandom(16))+"$")'
-    hashedPassword = "$QDn1BLMiGYFMQ";
+    hashedPassword = "$1$IwyaeK2S$mTlthniELh8N3fwLVZfPW0";
     openssh.authorizedKeys.keyFiles = [];
   };
 }
